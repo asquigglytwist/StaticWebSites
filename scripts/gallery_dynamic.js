c60c068762809c0141705def7040e025ed1feb32
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2014-12-26 14:51:58 +0530

    Making DynamicGallery release ready
    1)  Making DynamicGallery release ready - uncommented JS for checking
    XHR's return status code;
    2)  Making script adaptable to use any XML file for dynamic generation
    instead of a static one;
    3)  Wrapping the dynamic content in an Article instead of Div.

--- scripts/gallery_dynamic.js
+++ scripts/gallery_dynamic.js
@@ -3,12 +3,14 @@
 var MSN = MSN || {};
 MSN.iDelay = 7000;
 MSN.bAsyncRequest = true;
-MSN.sGalleryXML = "data/gallery.xml";
+MSN.sGalleryXML = undefined;
 MSN.sDivImages = "Images";
 MSN.sTagGallery = "dyngallery";
+MSN.sDOMWrappingTag = "article";
 MSN.ndDivImages = undefined;
 
-MSN.fnDynamicGallery = function () {
+MSN.fnDynamicGallery = function (sXMLFilePath) {
+	MSN.sGalleryXML = (sXMLFilePath ? sXMLFilePath : "data/gallery.xml");
 	MSN.ndDivImages = document.getElementById(MSN.sDivImages);
 	MSN.fnFetchDynamicXML();
 };
@@ -16,7 +18,7 @@ MSN.fnFetchDynamicXML = function () {
 	var xhr = new XMLHttpRequest();
 	xhr.onreadystatechange = function ()
 	{
-		if ((xhr.readyState == 4) /*&& (xhr.status == 200)*/)
+		if ((xhr.readyState == 4) && (xhr.status == 200))
 		{
 			if(!xhr.responseXML || xhr.responseXML.length)
 			{
@@ -26,7 +28,7 @@ MSN.fnFetchDynamicXML = function () {
 							allGalleryItems = ndGalRoot.children;
 			for(var i = 0; i < allGalleryItems.length; i++)
 			{
-				var ndTemp = document.createElement('div');
+				var ndTemp = document.createElement(MSN.sDOMWrappingTag);
 				ndTemp.innerHTML = allGalleryItems[i].outerHTML;
 				//MSN.ndDivImages.innerHTML += allGalleryItems[i].outerHTML;
 				MSN.ndDivImages.appendChild(ndTemp);
