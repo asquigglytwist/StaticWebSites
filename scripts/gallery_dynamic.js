81fce5ebf4820aeca9473f025c9e384a2f8adf21
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2015-01-07 01:13:53 +0530

    Adding support for Android Browser
    Adding support for Android's default Browser with webkit 533.1 where
    Gallery wasn't loading at all.  Change is a huge chunk and has to be
    reviewed since possibility of breaking is higher.

--- scripts/gallery_dynamic.js
+++ scripts/gallery_dynamic.js
@@ -20,18 +20,48 @@ MSN.fnFetchDynamicXML = function () {
 	{
 		if ((xhr.readyState == 4) && (xhr.status == 200))
 		{
-			if(!xhr.responseXML || xhr.responseXML.length)
+			if(!xhr.responseXML)// || xhr.responseXML.length)
 			{
 				return;
 			}
 			var ndGalRoot = xhr.responseXML.getElementsByTagName(MSN.sTagGallery)[0],
 							allGalleryItems = ndGalRoot.children;
-			for(var i = 0; i < allGalleryItems.length; i++)
+			if(allGalleryItems)
 			{
-				var ndTemp = document.createElement(MSN.sDOMWrappingTag);
-				ndTemp.innerHTML = allGalleryItems[i].outerHTML;
-				//MSN.ndDivImages.innerHTML += allGalleryItems[i].outerHTML;
-				MSN.ndDivImages.appendChild(ndTemp);
+				for(var i = 0; i < allGalleryItems.length; i++)
+				{
+					var ndTemp = document.createElement(MSN.sDOMWrappingTag);
+					ndTemp.innerHTML = allGalleryItems[i].outerHTML;
+					MSN.ndDivImages.appendChild(ndTemp);
+				}
+			}
+			else
+			{
+				if (window.DOMParser)
+				{
+					var xmlDoc = undefined;
+					parser = new DOMParser();
+					xmlDoc = parser.parseFromString(xhr.responseText, "text/xml");
+					ndGalRoot = xmlDoc.getElementsByTagName(MSN.sTagGallery)[0],
+					allGalleryItems = ndGalRoot.childNodes;
+					for(var i = 0; i < allGalleryItems.length; i++)
+					{
+						if(Node.ELEMENT_NODE != allGalleryItems[i].nodeType)
+							continue;
+						var ndTemp = document.createElement(MSN.sDOMWrappingTag);
+						var ndTitle = allGalleryItems[i].getElementsByTagName("h3")[0], ndDesc = allGalleryItems[i].getElementsByTagName("h4")[0];
+						ndTemp.appendChild(ndTitle.cloneNode(true));
+						var ndaImages = allGalleryItems[i].getElementsByTagName("img");
+						for(var j = 0; j < ndaImages.length; j++)
+						{
+							var ndImg = document.createElement("img");
+							ndImg.setAttribute('src', ndaImages[j].getAttribute('src'));
+							ndTemp.appendChild(ndImg);
+						}
+						ndTemp.appendChild(ndDesc.cloneNode(true));
+						MSN.ndDivImages.appendChild(ndTemp);
+					}
+				}
 			}
 		}
 	}
