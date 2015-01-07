927d14715bfc0ee030ad90bf5c012da256d01d11
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2015-01-07 23:57:34 +0530

    Code for reading and rendering new fields
    Code for reading and rendering new fields - the chief guest and minor
    corrections to old fields.

--- scripts/eventing.js
+++ scripts/eventing.js
@@ -9,6 +9,8 @@ MSN.sTagEvtTitle = "title";
 MSN.sTagEvtDesc = "desc";
 MSN.sTagEvtWhen = "when";
 MSN.sTagEvtWhere = "where";
+MSN.sTagEvtChiefGuestsList = "chiefguests";
+MSN.sTagEvtGuest = "guest";
 MSN.sTagToUse = "div";
 MSN.ndEventDivs = [];
 MSN.iDivIxToShow = 0;
@@ -24,71 +26,80 @@ MSN.fnIsIE = function () {
 MSN.fnGetNodeValue = function(ndNode) {
 	return ndNode.firstChild.nodeValue;
 };
-MSN.fnCreateTicker = function()
-{
-	MSN.ndTicker = document.getElementById("Ticker");
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function ()
-	{
-		if ((xhr.readyState == 4) && (xhr.status == 200))
-		{
-			if(!xhr.responseXML || xhr.responseXML.length)
-			{
-				MSN.ndTicker.innerHTML = "No upcoming events.";
-				MSN.ndTicker.style.display = "block";
-				MSN.ndTicker.style.opacity = 1;
-				return;
-			}
-			var allUpcomingEvents = xhr.responseXML.getElementsByTagName(MSN.sTagEvent);
-			for(var i = 0; i < allUpcomingEvents.length; i++)
-			{
-				var sTitle = MSN.fnGetNodeValue(allUpcomingEvents[i].getElementsByTagName(MSN.sTagEvtTitle)[0]),
+MSN.fnCreateTicker = function () {
+    MSN.ndTicker = document.getElementById("Ticker");
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if ((xhr.readyState == 4) && (xhr.status == 200)) {
+            if (!xhr.responseXML || xhr.responseXML.length) {
+                MSN.ndTicker.innerHTML = "No upcoming events.";
+                MSN.ndTicker.style.display = "block";
+                MSN.ndTicker.style.opacity = 1;
+                return;
+            }
+            var allUpcomingEvents = xhr.responseXML.getElementsByTagName(MSN.sTagEvent);
+            for (var i = 0; i < allUpcomingEvents.length; i++) {
+                var sTitle = MSN.fnGetNodeValue(allUpcomingEvents[i].getElementsByTagName(MSN.sTagEvtTitle)[0]),
 					sDesc = MSN.fnGetNodeValue(allUpcomingEvents[i].getElementsByTagName(MSN.sTagEvtDesc)[0]),
 					sWhen = MSN.fnGetNodeValue(allUpcomingEvents[i].getElementsByTagName(MSN.sTagEvtWhen)[0]),
 					sWhere = MSN.fnGetNodeValue(allUpcomingEvents[i].getElementsByTagName(MSN.sTagEvtWhere)[0]);
-				if(!sTitle)
-					sTitle = " ";
-				if(!sDesc)
-					sDesc = " ";
-				if(!sWhen)
-					sWhen = " ";
-				if(!sWhere)
-					sWhere = " ";
-				var ndEvtTitle = document.createElement("h2");
-				ndEvtTitle.innerHTML = sTitle;
-				var ndEvtDesc = document.createElement("h3");
-				ndEvtDesc.innerHTML = sDesc;
-				var ndEvtWhenWhere = document.createElement("h3");
-				ndEvtWhenWhere.innerHTML = ("on&nbsp;&nbsp;" + sWhen + "&nbsp;&nbsp;at&nbsp;&nbsp;" + sWhere);
-				ndEvtWhenWhere.setAttribute("class", "onat");
-				var ndTemp = document.createElement(MSN.sTagToUse);
-				ndTemp.appendChild(ndEvtTitle);
-				ndTemp.appendChild(ndEvtWhenWhere);
-				ndTemp.appendChild(ndEvtDesc);
-				MSN.fnHideNode(ndTemp);
-				MSN.ndEventDivs.push(ndTemp);
-				MSN.ndTicker.firstElementChild.innerHTML += ".";
-				MSN.ndTicker.appendChild(ndTemp);
-			}
-			MSN.chkTicker = document.getElementById("PauseTicker");
-			MSN.ndTicker.style.display = "block";
-			MSN.ndTicker.style.opacity = 1;
-			if(MSN.ndEventDivs.length > 0)
-			{
-				MSN.fnUpdateTicker();
-				if(MSN.fnOneTime)
-					MSN.fnOneTime();
-				MSN.tmrTicker = setInterval(function() { MSN.fnUpdateTicker() }, MSN.iDelay);
-			}
-			else
-			{
-				MSN.chkTicker.disabled = true;
-				MSN.ndTicker.innerHTML = "No upcoming events.";
-			}
-		}
-	};
-	xhr.open("GET", MSN.sUpComingXML, MSN.bAsyncRequest);
-	xhr.send();
+                if (!sTitle)
+                    sTitle = " ";
+                if (!sDesc)
+                    sDesc = " ";
+                if (!sWhen)
+                    sWhen = " ";
+                if (!sWhere)
+                    sWhere = " ";
+                var ndEvtTitle = document.createElement("h2");
+                ndEvtTitle.innerHTML = sTitle;
+                var ndEvtDesc = document.createElement("h3");
+                ndEvtDesc.innerHTML = sDesc;
+                var ndEvtWhenWhere = document.createElement("h3");
+                ndEvtWhenWhere.innerHTML = ("on&nbsp;&nbsp;" + sWhen + "&nbsp;&nbsp;at&nbsp;&nbsp;" + sWhere);
+                ndEvtWhenWhere.setAttribute("class", "onat");
+                var ndTemp = document.createElement(MSN.sTagToUse);
+                ndTemp.appendChild(ndEvtTitle);
+                ndTemp.appendChild(ndEvtWhenWhere);
+                var ndAllGuestsGroup = allUpcomingEvents[i].getElementsByTagName(MSN.sTagEvtChiefGuestsList)[0],
+                ndAllGuests = ndAllGuestsGroup ? ndAllGuestsGroup.getElementsByTagName(MSN.sTagEvtGuest) : [],
+                ndGuestsTitle = document.createElement('h3')
+                ndGuestList = document.createElement('ul');
+                ndGuestsTitle.innerHTML = "Chief Guests";
+                ndGuestsTitle.setAttribute('class', 'gueststitle')
+                for (var j = 0; j < ndAllGuests.length; j++) {
+                    var ndGuest = document.createElement('li');
+                    ndGuest.innerHTML = ndAllGuests[j].textContent;
+                    ndGuestList.appendChild(ndGuest);
+                }
+                if (ndAllGuests.length) {
+                    ndGuestList.setAttribute('class', 'chiefguest');
+                    ndTemp.appendChild(ndGuestsTitle);
+                    ndTemp.appendChild(ndGuestList);
+                }
+                ndTemp.appendChild(ndEvtDesc);
+                MSN.fnHideNode(ndTemp);
+                MSN.ndEventDivs.push(ndTemp);
+                MSN.ndTicker.firstElementChild.innerHTML += ".";
+                MSN.ndTicker.appendChild(ndTemp);
+            }
+            MSN.chkTicker = document.getElementById("PauseTicker");
+            MSN.ndTicker.style.display = "block";
+            MSN.ndTicker.style.opacity = 1;
+            if (MSN.ndEventDivs.length > 0) {
+                MSN.fnUpdateTicker();
+                if (MSN.fnOneTime)
+                    MSN.fnOneTime();
+                MSN.tmrTicker = setInterval(function () { MSN.fnUpdateTicker() }, MSN.iDelay);
+            }
+            else {
+                MSN.chkTicker.disabled = true;
+                MSN.ndTicker.innerHTML = "No upcoming events.";
+            }
+        }
+    };
+    xhr.open("GET", MSN.sUpComingXML, MSN.bAsyncRequest);
+    xhr.send();
 };
 MSN.fnUpdateTicker = function() {
 	MSN.fnHideNode(MSN.ndTicker);
