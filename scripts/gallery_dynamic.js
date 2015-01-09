d17de4eed12c1726c8f23b5841b320e0015250bb
ASquigglyTwist <9925933+asquigglytwist@users.noreply.github.com>
2015-01-09 20:52:29 +0530

    Gallery revamp
    1)  Making Gallery work in IE11 (hopefully solves Android Native browser
    issues as well) and corrected a few minor issues as well;
    2)  A better calling mechanism for event binding - referencing
    StaticGallery script in DynamicGallery and calling appropriate function
    after script loading is complete.

--- scripts/gallery_dynamic.js
+++ scripts/gallery_dynamic.js
@@ -15,63 +15,65 @@ MSN.fnDynamicGallery = function (sXMLFilePath) {
 	MSN.fnFetchDynamicXML();
 };
 MSN.fnFetchDynamicXML = function () {
-	var xhr = new XMLHttpRequest();
-	xhr.onreadystatechange = function ()
-	{
-		if ((xhr.readyState == 4) && (xhr.status == 200))
-		{
-			if(!xhr.responseXML)// || xhr.responseXML.length)
-			{
-				return;
-			}
-			var ndGalRoot = xhr.responseXML.getElementsByTagName(MSN.sTagGallery)[0],
+    var xhr = new XMLHttpRequest();
+    xhr.onreadystatechange = function () {
+        if ((xhr.readyState == 4) && (xhr.status == 200)) {
+            if (!xhr.responseXML)// || xhr.responseXML.length)
+            {
+                return;
+            }
+            var ndGalRoot = xhr.responseXML.getElementsByTagName(MSN.sTagGallery)[0],
 							allGalleryItems = ndGalRoot.children;
-			if(allGalleryItems)
-			{
-				for(var i = 0; i < allGalleryItems.length; i++)
-				{
-					var ndTemp = document.createElement(MSN.sDOMWrappingTag);
-					ndTemp.innerHTML = allGalleryItems[i].outerHTML;
-					MSN.ndDivImages.appendChild(ndTemp);
-				}
-			}
-			else
-			{
-				if (window.DOMParser)
-				{
-					MSN.sDOMWrappingTag = 'div';
-					var xmlDoc = undefined;
-					parser = new DOMParser();
-					xmlDoc = parser.parseFromString(xhr.responseText, "text/xml");
-					ndGalRoot = xmlDoc.getElementsByTagName(MSN.sTagGallery)[0], allGalleryItems = ndGalRoot.childNodes;
-					for(var i = 0; i < allGalleryItems.length; i++)
-					{
-						if(Node.ELEMENT_NODE != allGalleryItems[i].nodeType)
-							continue;
-						var ndWrap = document.createElement(MSN.sDOMWrappingTag), ndEvent = document.createElement('div');
-						ndEvent.setAttribute('class', 'event');
-						var ndTitle = ((allGalleryItems[i].getElementsByTagName("h3")[0]).cloneNode(true)), ndDesc = ((allGalleryItems[i].getElementsByTagName("h4")[0]).cloneNode(true));
-						ndTitle.setAttribute('class', 'eventtitle');
-						var ndTitlePara = document.createElement("p");
-						ndTitlePara.appendChild(ndTitle);
-						ndEvent.appendChild(ndTitlePara);
-						var ndaImages = allGalleryItems[i].getElementsByTagName("img");
-						var ndImgPara = document.createElement("p");
-						for(var j = 0; j < ndaImages.length; j++)
-						{
-							var ndImg = document.createElement("img");
-							ndImg.setAttribute('src', ndaImages[j].getAttribute('src'));
-							ndImg.setAttribute('class', 'thumb');
-							ndImgPara.appendChild(ndImg);
-						}
-						ndEvent.appendChild(ndImgPara);
-						ndEvent.appendChild(ndDesc);
-						MSN.ndDivImages.appendChild(ndEvent);
-					}
-				}
-			}
-		}
-	}
-	xhr.open("GET", MSN.sGalleryXML, MSN.bAsyncRequest);
-	xhr.send();
+            if (allGalleryItems) {
+                document.getElementById('LoadingMsg').innerHTML = "Loading Images...  Please wait.  We'll make sure it is worth the wait."
+                for (var i = 0; i < allGalleryItems.length; i++) {
+                    var ndTemp = document.createElement(MSN.sDOMWrappingTag);
+                    ndTemp.innerHTML = allGalleryItems[i].outerHTML;
+                    MSN.ndDivImages.appendChild(ndTemp);
+                }
+            }
+            else {
+                if (window.DOMParser) {
+                    document.getElementById('LoadingMsg').innerHTML = "Loading Images...  Please wait.  We'll make sure it is worth the wait."
+                    MSN.sDOMWrappingTag = 'div';
+                    var xmlDoc = undefined;
+                    parser = new DOMParser();
+                    xmlDoc = parser.parseFromString(xhr.responseText, "text/xml");
+                    ndGalRoot = xmlDoc.getElementsByTagName(MSN.sTagGallery)[0], allGalleryItems = ndGalRoot.childNodes;
+                    for (var i = 0; i < allGalleryItems.length; i++) {
+                        if (Node.ELEMENT_NODE != allGalleryItems[i].nodeType)
+                            continue;
+                        var ndWrap = document.createElement(MSN.sDOMWrappingTag), ndEvent = document.createElement('div');
+                        ndEvent.setAttribute('class', 'event');
+                        var ndTitle = ((allGalleryItems[i].getElementsByTagName("h3")[0]).cloneNode(true)), ndDesc = ((allGalleryItems[i].getElementsByTagName("h4")[0]).cloneNode(true)), ndDateAndOrganizer = ((allGalleryItems[i].getElementsByTagName("div")[0]).cloneNode(true)), ndVenue = ((allGalleryItems[i].getElementsByTagName("div")[1]).cloneNode(true));
+                        ndTitle.setAttribute('class', 'eventtitle');
+                        var ndTitlePara = document.createElement("p");
+                        ndTitlePara.appendChild(ndTitle);
+                        ndEvent.appendChild(ndTitlePara);
+                        ndEvent.appendChild(ndDateAndOrganizer);
+                        ndEvent.appendChild(ndVenue);
+                        var ndaImages = allGalleryItems[i].getElementsByTagName("img");
+                        var ndImgPara = document.createElement("p");
+                        for (var j = 0; j < ndaImages.length; j++) {
+                            var ndImg = document.createElement("img");
+                            ndImg.setAttribute('src', ndaImages[j].getAttribute('src'));
+                            ndImg.setAttribute('class', 'thumb');
+                            ndImgPara.appendChild(ndImg);
+                        }
+                        ndEvent.appendChild(ndImgPara);
+                        ndEvent.appendChild(ndDesc);
+                        MSN.ndDivImages.appendChild(ndEvent);
+                    }
+                }
+            }
+            var script = document.createElement('script');
+            script.src = "/scripts/gallery_static.js";
+            script.onload = function () {
+                MSN.fnStaticGallery();
+            };
+            document.head.appendChild(script);
+        }
+    };
+    xhr.open("GET", MSN.sGalleryXML, MSN.bAsyncRequest);
+    xhr.send();
 };
--- scripts/gallery_static.js
+++ scripts/gallery_static.js
@@ -6,7 +6,7 @@ MSN.ndGallery = undefined;
 MSN.sEventsClass = ".event";
 MSN.ndAllEvents = [];
 MSN.sEventTitleAttrib = "data-title";
-MSN.sEventTimeAttrib = "data-on";
+MSN.sEventDateAttrib = "data-on";
 MSN.sEventLocationAttrib = "data-at";
 MSN.sEventDescAttrib = "data-desc";
 MSN.sAllEventTitles = [];
@@ -48,7 +48,7 @@ MSN.fnLoadImage = function (ndTemp) {
 		MSN.ndFSTitle.innerHTML = "M.S.Natyalaya";
 	MSN.ndFSImage.setAttribute("data-imageid", ndTemp.getAttribute("data-imageid"));
 	var sTitle = ndTemp.getAttribute("title");
-	if(sTitle.length > 0)
+	if(sTitle && sTitle.length > 0)
 		MSN.ndFSCaption.innerHTML = sTitle;
 	else
 		MSN.ndFSCaption.innerHTML = "";
@@ -108,57 +108,68 @@ MSN.fnFSKeyUp = function (evt) {
 	}
 };
 MSN.fnStaticGallery = function () {
-	MSN.ndGallery = document.getElementById(MSN.sGalleryDivID);
-	MSN.ndAllEvents = MSN.ndGallery.querySelectorAll(MSN.sEventsClass);
-	var sImgPaths = "";
-	for(var i = 0; i < MSN.ndAllEvents.length; i++)
-	{
-		var sTitle = MSN.ndAllEvents[i].getAttribute(MSN.sEventTitleAttrib),
-			sTime = MSN.ndAllEvents[i].getAttribute(MSN.sEventTimeAttrib),
+    MSN.ndGallery = document.getElementById(MSN.sGalleryDivID);
+    MSN.ndAllEvents = MSN.ndGallery.querySelectorAll(MSN.sEventsClass);
+    var sImgPaths = "";
+    for (var i = 0; i < MSN.ndAllEvents.length; i++) {
+        var sTitle = MSN.ndAllEvents[i].getAttribute(MSN.sEventTitleAttrib),
+			sDate = MSN.ndAllEvents[i].getAttribute(MSN.sEventDateAttrib),
 			sLocation = MSN.ndAllEvents[i].getAttribute(MSN.sEventLocationAttrib),
 			sDesc = MSN.ndAllEvents[i].getAttribute(MSN.sEventDescAttrib);
+
+        if (!sTitle) {
+            sTitle = MSN.ndAllEvents[i].getElementsByTagName('h3')[0].textContent;
+        }
+        if (!sDate) {
+            sDate = ((MSN.ndAllEvents[i].getElementsByTagName('div')[0]).getElementsByTagName('span')[0]).textContent;
+        }
+        if (!sLocation) {
+            sLocation = ((MSN.ndAllEvents[i].getElementsByTagName('div')[1]).getElementsByTagName('span')[0]).textContent;
+        }
+        if (!sDesc) {
+            sDesc = MSN.ndAllEvents[i].getElementsByTagName('h4')[0].innerHTML;
+        }
+        MSN.sAllEventTitles.push(sTitle);
+        MSN.sAllEventTimes.push(sDate);
+        MSN.sAllEventLocations.push(sLocation);
+
+        /*var ndTitle = document.createElement("h3");
+        ndTitle.innerHTML = sTitle;
+        MSN.ndAllEvents[i].insertBefore(ndTitle, MSN.ndAllEvents[i].firstChild);
 		
-		MSN.sAllEventTitles.push(sTitle);
-		MSN.sAllEventTimes.push(sTime);
-		MSN.sAllEventLocations.push(sLocation);
+        var ndTime = document.createElement("span"), ndLoc = document.createElement("span");
+        ndTime.innerHTML = sDate;
+        ndLoc.innerHTML = sLocation;
+        var ndWhenWhere = document.createElement("div");
+        ndWhenWhere.innerHTML = "On ";
+        ndWhenWhere.appendChild(ndTime);
+        ndWhenWhere.innerHTML += " at ";
+        ndWhenWhere.appendChild(ndLoc);
+        //MSN.ndAllEvents[i].appendChild(ndWhenWhere);
+        MSN.ndAllEvents[i].insertBefore(ndWhenWhere, MSN.ndAllEvents[i].firstChild);
+        MSN.ndAllEvents[i].insertBefore(ndTitle, MSN.ndAllEvents[i].firstChild);
 		
-		/*var ndTitle = document.createElement("h3");
-		ndTitle.innerHTML = sTitle;
-		MSN.ndAllEvents[i].insertBefore(ndTitle, MSN.ndAllEvents[i].firstChild);
-		
-		var ndTime = document.createElement("span"), ndLoc = document.createElement("span");
-		ndTime.innerHTML = sTime;
-		ndLoc.innerHTML = sLocation;
-		var ndWhenWhere = document.createElement("div");
-		ndWhenWhere.innerHTML = "On ";
-		ndWhenWhere.appendChild(ndTime);
-		ndWhenWhere.innerHTML += " at ";
-		ndWhenWhere.appendChild(ndLoc);
-		//MSN.ndAllEvents[i].appendChild(ndWhenWhere);
-		MSN.ndAllEvents[i].insertBefore(ndWhenWhere, MSN.ndAllEvents[i].firstChild);
-		MSN.ndAllEvents[i].insertBefore(ndTitle, MSN.ndAllEvents[i].firstChild);
-		
-		var ndDesc = document.createElement("h4");
-		ndDesc.innerHTML = sDesc;
-		MSN.ndAllEvents[i].appendChild(ndDesc);*/
-		
-		var tmpAllImgs = MSN.ndAllEvents[i].getElementsByTagName("img");
-		for(var j = 0; j < tmpAllImgs.length; j++)
-		{
-			tmpAllImgs[j].setAttribute("data-eventid", i.toString());
-			tmpAllImgs[j].setAttribute("data-imageid", MSN.ndAllImgs.length.toString());
-			tmpAllImgs[j].onclick = MSN.fnFullScreen;
-			tmpAllImgs[j].src = tmpAllImgs[j].src ? tmpAllImgs[j].src : tmpAllImgs[j].getAttribute('src');
-			sImgPaths += ((MSN.fnGetFullImagePath(tmpAllImgs[j].src)) + ",");
-			MSN.ndAllImgs.push(tmpAllImgs[j]);
-		}
-	}
-	MSN.ndFullScreen = document.getElementById(MSN.sFullScreenDivID);
-	MSN.ndFSImage = document.getElementById(MSN.sFSImgID);
-	MSN.ndFSTitle = document.getElementById(MSN.sFSTitleID);
-	MSN.ndFSCaption = document.getElementById(MSN.sFSCaptionID);
-	if(typeof(Worker) !== "undefined") {
-		MSN.imgFetcher = new Worker('scripts/imgfetcher.js');
-		MSN.imgFetcher.postMessage(sImgPaths);
-	}
+        var ndDesc = document.createElement("h4");
+        ndDesc.innerHTML = sDesc;
+        MSN.ndAllEvents[i].appendChild(ndDesc);*/
+
+        var tmpAllImgs = MSN.ndAllEvents[i].getElementsByTagName("img");
+        for (var j = 0; j < tmpAllImgs.length; j++) {
+            tmpAllImgs[j].setAttribute("data-eventid", i.toString());
+            tmpAllImgs[j].setAttribute("data-imageid", MSN.ndAllImgs.length.toString());
+            tmpAllImgs[j].onclick = MSN.fnFullScreen;
+            tmpAllImgs[j].src = tmpAllImgs[j].src ? tmpAllImgs[j].src : tmpAllImgs[j].getAttribute('src');
+            sImgPaths += ((MSN.fnGetFullImagePath(tmpAllImgs[j].src)) + ",");
+            MSN.ndAllImgs.push(tmpAllImgs[j]);
+        }
+    }
+    MSN.ndFullScreen = document.getElementById(MSN.sFullScreenDivID);
+    MSN.ndFSImage = document.getElementById(MSN.sFSImgID);
+    MSN.ndFSTitle = document.getElementById(MSN.sFSTitleID);
+    MSN.ndFSCaption = document.getElementById(MSN.sFSCaptionID);
+    document.getElementById('LoadingMsg').style.display = "none";
+    if (typeof (Worker) !== "undefined") {
+        MSN.imgFetcher = new Worker('scripts/imgfetcher.js');
+        MSN.imgFetcher.postMessage(sImgPaths);
+    }
 };
